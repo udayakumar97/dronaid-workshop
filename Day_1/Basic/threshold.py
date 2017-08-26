b20974032788636690a01c23460d68b2e0ad7524
Dhruv <dhruvrnaik@gmail.com>
2017-08-27 02:23:39 +0530

    Basics-Day1

--- Day_1/Basic/threshold.py
+++ Day_1/Basic/threshold.py
@@ -7,10 +7,10 @@ cv2.imshow('1',img1)
 img2gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 
 #Threshold Function
-ret, mask = cv2.threshold(img2gray, 225, 255, cv2.THRESH_BINARY)
+ret, thresh = cv2.threshold(img2gray, 225, 255, cv2.THRESH_BINARY)
 
 
-cv2.imshow('Mask',mask)
+cv2.imshow('Thresh',thresh)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
